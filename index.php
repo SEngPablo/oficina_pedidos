a73c48d2698df5fe6ed3236805c307259d3d5d01
Pablo <152925963+SEngPablo@users.noreply.github.com>
2023-12-13 20:27:04 -0300

    Aplicação e inclusão de Bootstrap

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Pedidos</title>
-</head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="index.css">
+    </head>
+
 <body>
     <h1>Meus Pedidos</h1>
 
@@ -19,24 +22,67 @@
 
 <hr>
 
-<h2>Meus Pedidos</h2>
+<div
+    class="table-responsive"
+>
+    <table
+        class="table table-primary"
+    >
+        <thead>
+            <tr>
+                <th scope="col">Data</th>
+                <th scope="col">Cliente</th>
+                <th scope="col">Produto</th>
+                <th scope="col">Valor</th>
+                <th scope="col">Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="">
+                <td scope="row">23/11/2023</td>
+                <td>José</td>
+                <td>Mesa</td>
+                <td>23,00</td>
+                <td><a
+                    name="editar" id="editar" class="btn btn-warning" href="#"
+                    role="button"
+                    >Editar</a>
+                    <a
+                    name="excluir"
+                    id="excluir"
+                    class="btn btn-danger"
+                    href="#"
+                    role="button"
+                    >Excluir</a
+                >
+                </td>
+            </tr>
+            <tr class="">
+                <td scope="row">23/11/2023</td>
+                <td>José</td>
+                <td>Mesa</td>
+                <td>23,00</td>
+                <td><a
+                    name="editar"
+                    id="editar"
+                    class="btn btn-warning"
+                    href="#"
+                    role="button"
+                    >Editar</a>
+                    <a
+                    name="excluir"
+                    id="excluir"
+                    class="btn btn-danger"
+                    href="#"
+                    role="button"
+                    >Excluir</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 
-<table>
 
-    <thead>
-        <tr>
-            <th>Data</th>
-            <th>Cliente</th>
-            <th>Produto</th>
-            <th>Valor</th>
-            <th>Ações</th>
-        </tr>
-    </thead>
-
-</table>
-
-
-
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
