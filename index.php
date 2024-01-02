0dbea0cc661c4f48d64dae2a23fc1c3ced81066f
sengpablo <sengineerpablo@gmail.com>
2024-01-02 18:51:23 -0300

    Criado excluir/editar/salvar

--- index.php
+++ index.php
@@ -4,39 +4,65 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Pedidos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
     <h1>Meus Pedidos</h1>
-
     <form action="salvar.php" method="post">
+        <label for="data" class="form-label">Data: </label><input type="datetime-local" name="data" id="data">
+        <label for="cliente">Cliente: </label><input type="text" name="cliente" id="cliente">
+        <label for="produto">Produto: </label><input type="text" name="produto" id="produto">
+        <label for="valor">Valor: </label><input type="number" name="valor" id="valor">
+        <input type="submit" value="Salvar">
+    </form>
+    <hr>
 
-<label for='data'>Data:</label> <input type="datetime-local" name='data' id='data'>
-<label for = 'cliente'>Cliente:</label> <input type="text" name = 'cliente' id='cliente'>
-<label for='produto'>Produto:</label> <input type="text" name = 'produto' id='produto'>
-<label for="valor">Valor:</label> <input type="number" name='valor' id='valor'>
-<input type="submit" value = 'Salvar'>
-</form>
+<div
+    class="table-responsive"
+>
+    <table
+        class="table table-primary"
+    >
+        <thead>
+            <tr>
+                <th scope="col">Data</th>
+                <th scope="col">Cliente</th>
+                <th scope="col">Produto</th>
+                <th scope="col">Valor</th>
+                <th scope="col">Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="">
+                <td scope="row">23/11/2023</td>
+                <td>José</td>
+                <td>Mesa</td>
+                <td>R$ 23,00</td>
+                <td>
+                    <a name="editar" id="editar" class="btn btn-warning" href="#" role="button">Editar</a>
+                    <a name="excluir" id="excluir" class="btn btn-danger" href="#" role="button">Excluir</a>
+                </td>
+            </tr>
 
-<hr>
-
-<h2>Meus Pedidos</h2>
-
-<table>
-
-    <thead>
-        <tr>
-            <th>Data</th>
-            <th>Cliente</th>
-            <th>Produto</th>
-            <th>Valor</th>
-            <th>Ações</th>
-        </tr>
-    </thead>
-
-</table>
+            <tr class="">
+                <td scope="row">02/01/2024</td>
+                <td>João</td>
+                <td>Cadeira</td>
+                <td>R$55,00</td>
+                <td>                    <a name="editar" id="editar" class="btn btn-warning" href="#" role="button">Editar</a>
+                    <a name="excluir" id="excluir" class="btn btn-danger" href="#" role="button">Excluir</a></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 
 
 
 
+
+    
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
