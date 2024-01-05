e282fee4b9cf8bb4da4d16f02cc6613ca9a2ff01
sengpablo <sengineerpablo@gmail.com>
2024-01-04 21:02:17 -0300

    Adicionado código arquivos editar/excluir

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Pedidos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="index.css">
+</head>
+
 <body>
     <h1>Meus Pedidos</h1>
     <form action="salvar.php" method="post">
@@ -17,8 +20,10 @@
     </form>
     <hr>
 
-<div class="table-responsive">
-    <table class="table table-primary">
+    <div
+    class="table-responsive">
+    <table
+        class="table table-primary">
         <thead>
             <tr>
                 <th scope="col">Data</th>
@@ -28,46 +33,51 @@
                 <th scope="col">Ações</th>
             </tr>
         </thead>
-
-        <tbody>    
-
-    <?php
-    //Conecta ao banco de dados por PDO
-    $host = 'localhost';
-    $dbname = 'pedidos';
-    $username = 'root';
-    $password = '';
-
-    try {
-        $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    } catch (PDOException $e) {
-        die ("Erro ao conectar ao banco de dados:".$e->getMessage());
-    }
-
-    //Selecionar os dados da tabela pedidos  
-
-    $sql = "SELECT * FROM pedidos";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute();
-    
-    //Exibe os pedidos em uma tabela
-    while ($row=$stmt->fetch()) {
-        echo "<tr>";
-        echo "<td>".$row['data']."</td>";
-        echo "<td>".$row['cliente']."</td>";
-        echo "<td>".$row['produto']."</td>";
-        echo "<td>".$row['valor']."</td>";
-        echo "<td>";
-        echo "<a href='editar.php?id=".$row['id']."'>Editar</a>";
-        echo "<a href='excluir.php?id=".$row['id']."'>Excluir</a>";  
-        echo "</td>";
-        echo "</tr>";
-    }
-    ?>
-
+        <tbody>
+            <?php
+            // Conecta ao banco de dados usando PDO
+            $host = 'localhost';
+            $dbname = 'pedidos';
+            $username = 'root';
+            $password = '';
+            try {
+                $pdo = new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
+            } catch (PDOException $e) {
+                die ("Erro ao conectar ao banco de dados".$e->getMessage());
+            }
+            // Selecionar os dados da tabela pedidos
+            $sql ="SELECT * FROM pedidos";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute(); 
+            //Exibir os produtos em uma tabela
+            while ($row=$stmt->fetch()) {
+                echo "<tr>";
+                echo "<td>".$row['data']."</td>";
+                echo "<td>".$row['cliente']."</td>";
+                echo "<td>".$row['produto']."</td>";
+                echo "<td>".$row['valor']."</td>";
+                echo "<td>";
+                echo "<a href='editar.php?id=".$row['id']."'>Editar</a>";
+                echo "<a href='excluir.php?id=".$row['id']."'>Excluir</a>";
+                echo "</td>";
+                echo "</tr>";
+            }
+            ?>
+           
+    <!-- 
+           <td>
+               <a name='editar' id='editar' class='btn btn-warning' href='editar.php?id=' role='button1'>Editar</a>;
+               <a name="excluir" id="excluir" class="btn btn-danger" href="#" role="button">Excluir</a>
+            </td>
+            <tr class="">
+                <td scope="row">Item</td>
+                <td>Item</td>
+                <td>Item</td>
+            </tr> -->
         </tbody>
     </table>
-</div>    
+    </div>
+
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
--- salvar.php
+++ salvar.php
@@ -4,32 +4,41 @@ $host = 'localhost';
 $dbname = 'pedidos';
 $username = 'root';
 $password = '';
-
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
 } catch (PDOException $e) {
-    die("Erro ao conectar ao banco de dados:".$e->getMessage());
+    die ("Erro ao conectar ao banco de dados".$e->getMessage());
 }
-
-// Verificar se o formulário foi enviado
-    if($_SERVER['REQUEST_METHOD']=='POST') {
-        // Obtém os dados do pedido do formulário
-        $data = $_POST['data'];
-        $data = $_POST['cliente'];
-        $data = $_POST['produto'];
-        $data = $_POST['valor'];
-
-        // Insere os dados do pedido na tabela 'pedidos'
-    $sql="INSERT INTO pedidos (data, cliente, produto, valor) VALUES (:data,:cliente,:produto,:valor)";
+//verifica se os dados forem enviados
+if ($_SERVER['REQUEST_METHOD']=='POST') {
+    $data = $_POST['data'];
+    $cliente = $_POST['cliente'];
+    $produto = $_POST['produto'];
+    $valor = $_POST['valor'];
+    // inserir os dados na tabela pedidos
+    $sql = "INSERT INTO pedidos (data,cliente,produto,valor) VALUES (:data,:cliente,:produto,:valor)";
     $stmt=$pdo->prepare($sql);
     $stmt->bindValue(':data',$data);
     $stmt->bindValue(':cliente',$cliente);
     $stmt->bindValue(':produto',$produto);
     $stmt->bindValue(':valor',$valor);
     $stmt->execute();
-
-    // Redireciona para a página principal após salvar o pedido
-        header('Location:index.php');
-        exit();
-    }
+    //volta para a página inicial
+    header('Location:index.php'); 
+    exit();
+}
 ?>
+<!-- <!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Pedidos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>  
+</body>
+</html> -->
